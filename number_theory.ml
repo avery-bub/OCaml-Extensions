1cb84ff7c350b469357312025bab9560d74d69c7
Avery Bub <anb48@cornell.edu>
2014-12-24 12:53:20 -0500

    wrote function for modular multiplicative inverse

--- number_theory.ml
+++ number_theory.ml
@@ -1,9 +1,5 @@
-(* 
-  This type represents the expression in the euclidean algorithm.
-  For example, (2322, 654, 3, 360) would represent the expression
-  2322 = 654*3 + 360.
-*)
-type expression = int * int * int * int
+
+exception NotInvertible
 
 (* 
   This is a function that shown all of the steps of computing the
@@ -22,4 +18,38 @@ let rec euclidean_algorithm_steps (x : int) (y : int) : unit =
       euclidean_algorithm_steps y r
 
 
-let rec euclidean_algorithm 
+(* 
+  This is a function that calculates the GCD of its arguments.
+  PRE: x >= y; y not equal to zero.
+  POST: returns an integer that is the GCD of x and y
+*)
+let rec euclidean_algorithm (x : int) (y : int) : int =
+  match x mod y with
+    | 0 -> y
+    | _ ->
+      let t = x/y in 
+      let r = x mod (t * y) in 
+      euclidean_algorithm y r 
+
+
+(* 
+  This is a function that computes the modular multiplicative inverse. For example,
+  if we want to find the inverse of 17 mod 24, the arguments would be:
+  x = 17
+  m = 24
+  t0 = 0
+  t1 = 1
+  r0 = 24
+  r1 = 17.
+  Admittedly, this seems a little complicated. However:
+  t0 will ALWAYS be entered as 0, t1 will ALWAYS be entered as 1, r0 will
+  ALWAYS be entered as the mod m, and r1 will ALWAYS be enetered as the x value.
+  So, all you really have to choose is the number being inversed (x) and its mod (m)
+*)
+let rec modular_multiplicative_inverse (x : int) (m : int) (t0 : int) (t1 : int) 
+  (r0 : int) (r1 : int) : int =
+    match r1 with
+      | 0 -> if r0 > 1 then raise NotInvertible else t0 + m 
+      | _ ->
+        let q = r0/r1 in 
+        modular_multiplicative_inverse x m t1 (t0 - (q * t1)) r1 (r0 - (q * r1))
